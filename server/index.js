e05562daf7653c1a32aee00e60fb365da0d97ce7
Adam Sinning <asinning25@gmail.com>
2023-08-16 17:45:20 -0600

    authorization get request added to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+const router = express.Router(); 
+
 const port = 5000
 
 dotenv.config()
@@ -9,6 +11,8 @@ var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
 var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
 const app = express();
+app.use(router); 
+
 
 app.get('/auth/login', (req, res) => {
 });
@@ -18,4 +22,33 @@ app.get('/auth/callback', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
+})
+
+var generateRandomString = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+router.get('/auth/login', (req, res) => {
+
+var scope = "streaming \
+                user-read-email \
+                user-read-private"
+
+var state = generateRandomString(16);
+
+var auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: spotify_client_id,
+    scope: scope,
+    redirect_uri: "http://localhost:5173/auth/callback",
+    state: state
+})
+
+res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
 })
