ca704547e0f04ccfa526649eeba47a8a1ce4d345
Adam Sinning <asinning25@gmail.com>
2023-08-16 18:54:41 -0600

    added login and playback components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,61 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import WebPlayback from './components/webplayback';
+import Login from './components/login';
 import './App.css'
 
 function App() {
 
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+
+    async function getToken() {
+      const response = await fetch('/auth/token');
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+
+    getToken();
+
+  }, []);
+
+  const [player, setPlayer] = useState(undefined);
+
+  useEffect(() => {
+
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+
+        const player = new window.Spotify.Player({
+            name: 'Web Playback SDK',
+            getOAuthToken: cb => { cb(props.token); },
+            volume: 0.5
+        });
+
+        setPlayer(player);
+
+        player.addListener('ready', ({ device_id }) => {
+            console.log('Ready with Device ID', device_id);
+        });
+
+        player.addListener('not_ready', ({ device_id }) => {
+            console.log('Device ID has gone offline', device_id);
+        });
+
+
+        player.connect();
+
+    };
+}, []);
+
   return (
     <>
-      <div className="everything">
-        Singo
-      </div>
+      { (token === '') ? <Login/> : <WebPlayback token={token} /> }
     </>
   )
 }
