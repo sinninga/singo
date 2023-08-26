b09a86ffac5138f9364ad4ca8972bd0531167929
Adam Sinning <asinning25@gmail.com>
2023-08-26 14:08:12 -0600

    server code refactored

--- server/index.js
+++ server/index.js
@@ -1,98 +1,14 @@
 import express from 'express';
-import request from 'request';
 import cors from 'cors'; 
-import { spotify_client_id, spotify_client_secret } from './spotifyConfig.js';
-
-
-const router = express.Router(); 
+import router from './spotifyAuth.js'; 
 
 const port = 5000
-
 const app = express();
+
 app.use(cors());
 app.use(router); 
-
-var access_token = "";
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
 
-
-console.log("up and running");
-var generateRandomString = function (length) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  };
-
-app.get('/test', (req, res) => {
-  console.log('test route hit');
-  res.send('Test route response');
-});
-
-router.get('/auth/login', (req, res) => {
-
-  console.log('/auth/login endpoint accessed via router');
-
-
-var scope = "streaming \
-                user-read-email \
-                user-read-private \
-                user-read-playback-state \
-                user-modify-playback-state"
-
-var state = generateRandomString(16);
-
-var auth_query_parameters = new URLSearchParams({
-    response_type: "code",
-    client_id: spotify_client_id,
-    scope: scope,
-    redirect_uri: "http://localhost:5000/auth/callback",
-    state: state
-})
-
-res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
-})
-
-app.get('/auth/callback', (req, res) => {
-
-  console.log('/auth/callback endpoint accessed via app');
-
-
-    var code = req.query.code;
-  
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: "http://localhost:5000/auth/callback",
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
-        'Content-Type' : 'application/x-www-form-urlencoded'
-      },
-      json: true
-    };
-  
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        access_token = body.access_token;
-        res.redirect('http://localhost:5173/')
-      }
-    });
-  });
-
-  app.get('/auth/token', (req, res) => {
-    console.log('/auth/token endpoint accessed');
-
-    res.json(
-       {
-          access_token: access_token
-       })
-  })
