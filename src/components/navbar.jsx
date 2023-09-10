413b47f8a20b9d18befa51b6ec6ab1e76b1a4468
Adam Sinning <asinning25@gmail.com>
2023-09-09 22:07:44 -0600

    navbar link removed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
       <ul className="nav-links">
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
     </nav>
