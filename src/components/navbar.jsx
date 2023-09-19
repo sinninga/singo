08e413a2b3c7f382176d0f537b3eb1dc5f1b2d70
Adam Sinning <asinning25@gmail.com>
2023-09-18 20:56:34 -0600

    navbar links updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="logo">SLiNGO</div>
       <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
         <li><a href="#">Flashcards</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
       </ul>
     </nav>
   );
