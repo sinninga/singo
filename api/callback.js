3168a57ac2723c0a8ea1a95a39b22f09f4db7ff0
Adam Sinning <asinning25@gmail.com>
2023-10-23 17:32:03 -0600

    redirect url update

--- api/callback.js
+++ api/callback.js
@@ -24,8 +24,7 @@ export default async function handler(req, res) {
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
         access_token = body.access_token;
-        // res.redirect('/api/token?access_token=' + body.access_token);
-        res.redirect(`/?access_token=${body.access_token}`);
+        res.redirect('/')
       }
     });
 }
