6fa5ef99361aa8f1f06e1215d09d4b95a6f121c1
Adam Sinning <asinning25@gmail.com>
2023-10-23 17:49:43 -0600

    revert last commit

--- api/token.js
+++ api/token.js
@@ -1,7 +1,6 @@
-const { access_token } = req.query;
-console.log('Received access_token:', access_token);
-
 export default async function handler(req, res) {
+    const { access_token } = req.query;
+    console.log('Received access_token:', access_token);
     if (!access_token) {
         return res.status(400).json({ error: 'Access token is missing.' });
     }
@@ -11,5 +10,5 @@ export default async function handler(req, res) {
         });
 
     console.log('Response sent:', { access_token });
-
+    
 }
