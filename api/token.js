706b5050e4674a1b2869bdc212bcd27a2cbfbdcd
Adam Sinning <asinning25@gmail.com>
2023-10-23 17:05:09 -0600

    added logs for debugging

--- api/token.js
+++ api/token.js
@@ -1,10 +1,14 @@
 export default async function handler(req, res) {
     const { access_token } = req.query;
+    console.log('Received access_token:', access_token);
     if (!access_token) {
         return res.status(400).json({ error: 'Access token is missing.' });
     }
     res.json(
         {
            access_token: access_token
-        })
+        });
+
+    console.log('Response sent:', { access_token });
+    
 }
