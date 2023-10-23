0cf6e9056db29cc62fc394d1ccc16b32bce91817
Adam Sinning <asinning25@gmail.com>
2023-10-23 15:21:34 -0600

    token.js added

--- api/login.js
+++ api/login.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         response_type: "code",
         client_id: SPOTIFY_CLIENT_ID,
         scope: scope,
-        redirect_uri: "https://slingo.vercel.app/auth/callback",
+        redirect_uri: "https://slingo.vercel.app/api/callback",
         state: state
     })
 
---
+++ api/token.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+    res.json(
+        {
+           access_token: access_token
+        })
+}
