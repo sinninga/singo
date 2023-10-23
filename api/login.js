b3346392ec7317a3364ad1f14bd6a81357144d7b
Adam Sinning <asinning25@gmail.com>
2023-10-23 14:51:04 -0600

    generateRandomString function added to login.js

--- api/login.js
+++ api/login.js
@@ -1,3 +1,13 @@
+var generateRandomString = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
 export default async function handler(req, res) {
     var scope = "streaming \
                 user-read-email \
