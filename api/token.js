796830587e9caa25d7c90254ba3ed3d9ea96ab69
Adam Sinning <asinning25@gmail.com>
2023-10-23 17:44:45 -0600

    changed scope of access token

--- api/token.js
+++ api/token.js
@@ -1,6 +1,7 @@
+const { access_token } = req.query;
+console.log('Received access_token:', access_token);
+
 export default async function handler(req, res) {
-    const { access_token } = req.query;
-    console.log('Received access_token:', access_token);
     if (!access_token) {
         return res.status(400).json({ error: 'Access token is missing.' });
     }
@@ -10,5 +11,5 @@ export default async function handler(req, res) {
         });
 
     console.log('Response sent:', { access_token });
-    
+
 }
