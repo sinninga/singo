d452fb87e6277804c13b34f7368e1dc79d03b6cb
Adam Sinning <asinning25@gmail.com>
2023-10-24 00:53:14 -0600

    redirect instead of res

--- api/token.js
+++ api/token.js
@@ -4,10 +4,10 @@ export default async function handler(req, res) {
     if (!access_token) {
         return res.status(400).json({ error: 'Access token is missing.' });
     }
-    res.json(
-        {
-           access_token: access_token
-        });
+    // res.json(
+    //     {
+    //        access_token: access_token
+    //     });
 
     res.redirect('/');
 
