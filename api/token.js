560558d6776da9c5dad50919e8ba26e637f01f29
Adam Sinning <asinning25@gmail.com>
2023-10-24 00:48:42 -0600

    redirect url update

--- api/token.js
+++ api/token.js
@@ -4,6 +4,11 @@ export default async function handler(req, res) {
     if (!access_token) {
         return res.status(400).json({ error: 'Access token is missing.' });
     }
-    res.redirect(`/api/token`);
-    
+    res.json(
+        {
+           access_token: access_token
+        });
+
+    res.redirect('/');
+
 }
