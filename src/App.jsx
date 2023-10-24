c4a73559eeea73b0f1ac8ee55a16d0b095c2f1e7
Adam Sinning <asinning25@gmail.com>
2023-10-23 23:27:44 -0600

    fetch url fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
 
     async function getToken() {
-      const response = await fetch('/api/token);
+      const response = await fetch('/api/token');
       console.log("Response:", response);
       const json = await response.json();
       setToken(json.access_token);
