6e4a4f76ba820947ad463c7f53a81a04a97703e0
Adam Sinning <asinning25@gmail.com>
2023-10-26 13:16:31 -0600

    translate.js update

--- api/translate.js
+++ api/translate.js
@@ -3,42 +3,41 @@ const { DEEPL_API_KEY } = process.env;
 
 let deepl_api_key = DEEPL_API_KEY;
 
-const router = express.Router();
-
-router.post('/translate', async (req, res) => {
-    try {
+export default async (req, res) => {
+    if (req.method === 'POST') {
+      try {
         const { text, targetLang } = req.body;
-
+  
         const deepLApiURL = 'https://api-free.deepl.com/v2/translate';
-
+  
         const translationRequest = {
-            text,
-            target_lang: targetLang,
+          text,
+          target_lang: targetLang,
         };
-
+        
         const response = await fetch(deepLApiURL, {
             method: 'POST',
             headers: {
-                'Authorization': `DeepL-Auth-Key ${deepl_api_key}`,
-                'Content-Type': 'application/x-www-form-urlencoded',
+              'Authorization': `DeepL-Auth-Key ${deepl_api_key}`,
+              'Content-Type': 'application/x-www-form-urlencoded',
             },
             body: new URLSearchParams(translationRequest).toString(),
-        });
-
-        if (response.ok) {
+          });
+    
+          if (response.ok) {
             const data = await response.json();
             const translatedText = data.translations[0].text;
             res.json({ translatedText });
-        } else {
+          } else {
             console.error('Error translating text:', response.statusText);
             res.status(500).json({ error: 'Internal Server Error' });
+          }
+        } catch (error) {
+          console.error('Error translating text:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
         }
-    } catch (error) {
-        console.error('Error translating text:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-export default router;
-
+      } else {
+        res.status(405).end(); // Method Not Allowed
+      }
+    };
 
