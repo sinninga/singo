0b46321c811d608baaa799ddd20229f4a5f2f675
Adam Sinning <asinning25@gmail.com>
2023-10-31 16:30:18 -0600

    config update with new url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://slingo.vercel.app/',
+  base: 'https://slingotest.vercel.app/',
   plugins: [react()],
 })
